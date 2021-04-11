188c07a9a095cf6215a21cbccfa2bffd1eb0baaa
Martu <martuu.amengual@gmail.com>
2021-04-11 17:47:49 -0300

    adding better predict and training Ia

--- predict.py
+++ predict.py
@@ -7,12 +7,33 @@ from keras.models import load_model
 cnn=load_model('modelo.h5')
 cnn.load_weights('pesos.h5')
 
-x=load_img('examples/futball.jpg', target_size=(21, 28))
-x=img_to_array(x)
+x=load_img('examples/golf.jpg', target_size=(21, 28))
+x=img_to_array(x)/255
 x=np.expand_dims(x, axis=0)
 arreglo=cnn.predict(x)
 resultado=arreglo[0]
 respuesta=np.argmax(resultado)
-print()
+percentage=resultado[respuesta]*100
+print(percentage)
+
+
 if respuesta==0:
     print('tenis')
+if respuesta==1:
+    print('americano')
+if respuesta==2:
+    print('f1')
+if respuesta==3:
+    print('natacion')
+if respuesta==4:
+    print('futbol')
+if respuesta==5:
+    print('ciclismo')
+if respuesta==6:
+    print('golf')
+if respuesta==7:
+    print('basket')
+if respuesta==8:
+    print('boxeo')
+if respuesta==9:
+    print('beisball')
